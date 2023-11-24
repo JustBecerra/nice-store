78d4be8f916a18bd1b55a0d49821f6160e62d365
JustBecerra <justj.becerra@gmail.com>
2023-11-24 15:48:47 -0300

    remove drawer and add icons for cart and profile

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,18 +1,32 @@
-import { AppBar, IconButton, Toolbar } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, Box, Toolbar } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
 export const NavBar = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
+      <Toolbar
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "0.5rem",
+          justifyContent: "flex-end",
+          width: "auto",
+        }}
+      >
+        <AccountBoxIcon />
+        <ShoppingCartIcon />
+        {/* <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                marginTop: "1rem",
+                marginX: "0.5rem",
+                gap: "0.25rem",
+              }}
+            >
+              <LogoutIcon />
+              <Typography>Log Out</Typography>
+            </Box> */}
       </Toolbar>
     </AppBar>
   );
