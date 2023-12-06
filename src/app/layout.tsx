f9cf6bc34dfd85f6323c6d262850a40a0025e861
JustBecerra <justj.becerra@gmail.com>
2023-12-06 13:12:51 -0300

    add session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReduxProvider } from "@/redux/provider";
 import { MUIThemeProvider } from "../../utils/provider";
-
+import SessionProvider from "../components/SessionProvider";
+import { getServerSession } from "next-auth";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -11,19 +12,20 @@ export const metadata: Metadata = {
   description: "A very nice store",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession();
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <SessionProvider session={pageProps.pageProps}> */}
-        <ReduxProvider>
-          <MUIThemeProvider>{children}</MUIThemeProvider>
-        </ReduxProvider>
-        {/* </SessionProvider> */}
+        <SessionProvider session={session}>
+          <ReduxProvider>
+            <MUIThemeProvider>{children}</MUIThemeProvider>
+          </ReduxProvider>
+        </SessionProvider>
       </body>
     </html>
   );
