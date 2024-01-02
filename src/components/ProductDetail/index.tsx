41244bdc7ecd7c3a37df0a2266656f81f02c5f8a
JustBecerra <justj.becerra@gmail.com>
2024-01-02 14:35:07 -0300

    add product detail responsiveness

--- src/components/ProductDetail/index.tsx
+++ src/components/ProductDetail/index.tsx
@@ -13,7 +13,7 @@ interface props {
 }
 export const ProductDetail = ({ item }: props) => {
   const dispatch = useDispatch();
-  const { title, price, image, description, category } = item;
+  const { title, price, image, description, category, rating } = item;
   const handleDispatch = () => {
     dispatch(addProducts(item));
   };
@@ -25,8 +25,9 @@ export const ProductDetail = ({ item }: props) => {
         flexDirection: "column",
         height: "100vh",
         gap: "0.5rem",
-        overflow: "scroll",
-        justifyContent: "space-between",
+        overflow: { mobile: "scroll", laptop: "hidden" },
+        justifyContent: { mobile: "space-between", laptop: "unset" },
+        alignItems: "center",
       }}
     >
       <AppBar position="static">
@@ -42,74 +43,117 @@ export const ProductDetail = ({ item }: props) => {
           <BackArrow />
         </Toolbar>
       </AppBar>
-      <Box sx={{ px: "0.5rem" }}>
-        <Image
-          src={image}
-          width={400}
-          height={300}
-          layout="responsive"
-          alt={""}
-        />
-      </Box>
       <Box
         sx={{
           display: "flex",
-          flexDirection: "column",
+          flexDirection: { mobile: "column", laptop: "row" },
+          justifyContent: "center",
+          alignItems: "center",
           gap: "1rem",
-          px: "0.5rem",
         }}
       >
-        <Typography
-          sx={{ fontSize: "1.5rem", color: `${theme.palette.primary.light}` }}
-        >
-          {title}
-        </Typography>
-
-        <Typography
+        <Box
           sx={{
-            fontSize: "1.25rem",
-            color: `${theme.palette.primary.light}`,
+            position: "relative",
+            width: { mobile: "98%", laptop: "70%" },
+            height: { laptop: "800px" },
+            paddingBottom: { mobile: "160%", laptop: "0" },
+            overflow: "hidden",
+            borderRadius: "0.75rem",
+            marginLeft: { laptop: "2rem" },
+            marginTop: { laptop: "0.5rem" },
           }}
         >
-          Category: {category}
-        </Typography>
-        <Typography
+          <Image src={image} layout="fill" objectFit="cover" alt={""} />
+        </Box>
+        <Box
           sx={{
-            fontSize: "1.25rem",
-            color: `${theme.palette.primary.light}`,
-            marginRight: "1.5rem",
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            height: "90%",
+            gap: "1rem",
+            px: "0.5rem",
+            justifyContent: "space-around",
+            alignItems: "center",
+            border: { laptop: `1px solid ${theme.palette.primary.light}` },
+            borderRadius: "0.75rem",
+            mr: "0.5rem",
           }}
         >
-          Price: ${price}
-        </Typography>
-
-        <Typography
-          sx={{
-            fontSize: "1rem",
-            color: `${theme.palette.primary.light}`,
-          }}
-        >
-          {description}.
-        </Typography>
-        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-          <Button
+          <Typography
             sx={{
-              border: `1px solid ${theme.palette.primary.light}`,
-              borderRadius: "0.75rem",
-              mb: "1rem",
-              mr: "0.5rem",
+              fontSize: { mobile: "1.5rem", laptop: "3rem" },
+              color: `${theme.palette.primary.light}`,
+              textAlign: { mobile: "unset", laptop: "center" },
             }}
-            onClick={handleDispatch}
+          >
+            {title}
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "1rem",
+              marginLeft: { laptop: "1rem" },
+            }}
           >
             <Typography
               sx={{
-                fontSize: "1rem",
+                fontSize: { mobile: "1.25rem", laptop: "1.75rem" },
                 color: `${theme.palette.primary.light}`,
               }}
             >
-              Add to cart
+              Category: {category}
             </Typography>
-          </Button>
+            <Typography
+              sx={{
+                fontSize: { mobile: "1.25rem", laptop: "1.75rem" },
+                color: `${theme.palette.primary.light}`,
+              }}
+            >
+              Rating: {rating.rate}/5 (out of {rating.count} rates)
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: { mobile: "1.25rem", laptop: "1.75rem" },
+                color: `${theme.palette.primary.light}`,
+              }}
+            >
+              Price: ${price}
+            </Typography>
+
+            <Typography
+              sx={{
+                fontSize: { mobile: "1rem", laptop: "1.5rem" },
+                color: `${theme.palette.primary.light}`,
+              }}
+            >
+              {description}.
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              sx={{
+                border: `1px solid ${theme.palette.primary.light}`,
+                backgroundColor: theme.palette.primary.main,
+                borderRadius: "0.75rem",
+                mb: "1rem",
+                mr: "0.5rem",
+                width: { laptop: "15rem" },
+              }}
+              onClick={handleDispatch}
+            >
+              <Typography
+                sx={{
+                  fontSize: "1rem",
+                  color: `${theme.palette.primary.light}`,
+                }}
+              >
+                Add to cart
+              </Typography>
+            </Button>
+          </Box>
         </Box>
       </Box>
     </Box>
