0d1bcaec37242d399eb73cb316df0cae6ab4736a
JustBecerra <justj.becerra@gmail.com>
2024-01-17 15:28:21 -0300

    correct sql query

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,10 +1,15 @@
 import { NextResponse } from "next/server";
-
+import { hash } from "bcrypt";
+import { sql } from "@vercel/postgres";
 export async function POST(request: Request) {
   try {
     const { email, password } = await request.json();
     //validate email and password
-
+    const hashedPassword = await hash(password, 10);
+    const response = await sql`
+    INSERT INTO USERS (email, password)
+    VALUES (${email}, ${hashedPassword})
+    `;
     console.log({ email, password });
   } catch (e) {
     console.log({ e });
